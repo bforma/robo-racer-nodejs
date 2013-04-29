ecad4f7770b2d19db3335be832bcbdc908aaa6b2
Bob Forma <bobforma@gmail.com>
2013-04-29 22:37:46 +0200

    - npm prune (removed ejs node module) - moved client side app sources to separate directory (public/app) to make it easier to include all of it while running tests - setup karma - wrote single test to see if karma is setup correctly

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,13 @@
+describe('Robo Racer controllers', function() {
+
+    describe('GameCtrl', function(){
+
+        it('should initialize an empty players map', function() {
+            var socket = {on: function(name, func) {}};
+            var scope = {},
+                ctrl = new GameCtrl(scope, socket);
+
+            expect(_.size(scope.players)).toBe(0);
+        });
+    });
+});
